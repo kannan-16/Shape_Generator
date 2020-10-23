61b356c471af0b2b516554e76847c3a61895bdf8
kannan-16 <57753993+kannan-16@users.noreply.github.com>
2020-10-23 21:06:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,24 +4,25 @@ var oldValue = 0;
 
 button.onclick = () => {
     let n = document.getElementById('number').value;
-    let choice = document.getElementById("choice").value;
-    choice = parseInt(choice);
+    let circle = document.getElementById('circle')
+    let square = document.getElementById('square')
+    let rect = document.getElementById('rectangle')
     // console.log(choice)
     n = parseInt(oldValue) + parseInt(n);
     // console.log(n);
     for (; i <= n; i++) {
         var box = document.createElement('div');
-        if (choice === 1) {
+        if (square.checked) {
             box.classList.add('square');
             document.getElementById('box').appendChild(box);
             box.innerHTML += i;
             oldValue = document.getElementById('box').lastElementChild.innerHTML;
-        } else if (choice === 2) {
+        } else if (circle.checked) {
             box.classList.add('circle');
             document.getElementById('box').appendChild(box);
             box.innerHTML += i;
             oldValue = document.getElementById('box').lastElementChild.innerHTML;
-        } else if (choice === 3) {
+        } else if (rect.checked) {
             box.classList.add('rectangle');
             document.getElementById('box').appendChild(box);
             box.innerHTML += i;
