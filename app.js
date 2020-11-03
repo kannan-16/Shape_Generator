dfe4d1d19cb5861d97dcda9e60c6d8f363eb178a
kannan-16 <57753993+kannan-16@users.noreply.github.com>
2020-11-03 13:43:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-let button = document.getElementById("theBoxes");
-var i = 1;
-var oldValue = 0;
+const button = document.getElementById("button")
+
+const circle = document.getElementById("circle")
+const square = document.getElementById("square")
+const rectangle = document.getElementById("rectangle")
+
+const box = document.getElementById("box")
+
+let oldValue = 0
+let numberInTheShape = 1
 
 button.onclick = () => {
-    let n = document.getElementById('number').value;
-    let circle = document.getElementById('circle')
-    let square = document.getElementById('square')
-    let rect = document.getElementById('rectangle')
-    // console.log(choice)
-    n = parseInt(oldValue) + parseInt(n);
-    // console.log(n);
-    for (; i <= n; i++) {
-        var box = document.createElement('div');
-        if (square.checked) {
-            box.classList.add('square');
-            document.getElementById('box').appendChild(box);
-            box.innerHTML += i;
-            oldValue = document.getElementById('box').lastElementChild.innerHTML;
-        } else if (circle.checked) {
-            box.classList.add('circle');
-            document.getElementById('box').appendChild(box);
-            box.innerHTML += i;
-            oldValue = document.getElementById('box').lastElementChild.innerHTML;
-        } else if (rect.checked) {
-            box.classList.add('rectangle');
-            document.getElementById('box').appendChild(box);
-            box.innerHTML += i;
-            oldValue = document.getElementById('box').lastElementChild.innerHTML;
-        } else {
-            document.write("invalid Input");
+
+    let n = document.getElementById("number").value;
+    n = Number(oldValue) + Number(n)
+
+    for (let j = numberInTheShape; j <= n; j++) {
+        var shape = document.createElement("div")
+        shape.innerHTML += numberInTheShape;
+        if (circle.checked) {
+            shape.classList.add("circle")
+        } else if (square.checked) {
+            shape.classList.add("square")
+        } else if (rectangle.checked) {
+            shape.classList.add("rectangle")
         }
+        box.appendChild(shape);
+        numberInTheShape++;
+        oldValue = box.lastElementChild.innerHTML;
     }
 }
